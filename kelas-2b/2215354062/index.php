5de0052ee8931be417747b1370e8dab9d234e00a
DavidSastrawan <davidsastrawan40@gmail.com>
2023-05-23 10:01:21 +0800

    menambahkan nama, nim, kelas

--- kelas-2b/2215354062/index.php
+++ kelas-2b/2215354062/index.php
@@ -5,5 +5,22 @@
 <body>
     <b>Semangat Belajar Web Dasar</b>
     <hr/>
+    <table>
+        <tr>
+            <td>Nama</td>
+            <td>:</td>
+            <td>I Gede David Sastrawan</td>
+        </tr>
+        <tr>
+            <td>NIM</td>
+            <td>:</td>
+            <td>2215354062</td>
+        </tr>
+        <tr>
+            <td>Kelas</td>
+            <td>:</td>
+            <td>2B</td>
+        </tr>
+    </table>
 </body>
 </html>

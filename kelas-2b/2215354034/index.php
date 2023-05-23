3ba7acff76c4552b365f53afe296988c72c29d9d
wdysarid <widyasaridamayanti@gmail.com>
2023-05-23 10:01:00 +0800

    Belajar Web Dasar

---
+++ kelas-2b/2215354034/index.php
@@ -0,0 +1,48 @@
+<html>
+<head>
+    <title>Latihan1</title>
+</head>
+<body>
+    <b>Tabel dan Gambar di HTML</b><br/>
+    <hr/>
+    <b>Membuat Tabel</b>
+    <br/>
+    <table border="1" width="100%">
+        <tr>
+            <th>NO</th>
+            <th>NIM</th>
+            <th>Nama Mahasiswa</th>
+            <th>Jurusan</th>
+        </tr>
+        <tr>
+            <td align="center">1</td>
+            <td>23001</td>
+            <td>Made Pradnyana Ambara</td>
+            <td align="center">TRPL</td>
+        </tr>
+        <tr>
+            <td align="center">2</td>
+            <td>23002</td>
+            <td>Putu Agus Putra Sedana</td>
+            <td align="center">MI</td>
+        </tr>
+        <tr>
+            <td colspan="3" align ="center"><b> Jumlah Mahasiswa<b></td>
+            <td align = "center"> <b>2 Orang<b></td>
+        </tr>
+    </table>
+
+    <hr/>
+    <b>Gambar Di HTML</b> <br/>
+     <marquee>
+    <img src="poltekbali.jpeg" height = "200" width="320"/>
+    </marquee> <br/> <br/>
+    <marquee direction="right">
+    <img src="suluban.jpeg" height = "200" width="320"/>
+    </marquee>
+
+    <hr/>
+    <b>Google Maps</b> <br/>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3942.861126562478!2d115.15949731457198!3d-8.799115193680517!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd244c13ee9d753%3A0x6c05042449b50f81!2sPoliteknik%20Negeri%20Bali!5e0!3m2!1sid!2sid!4v1679272607708!5m2!1sid!2sid" width="100%" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+</body>
+</html>
